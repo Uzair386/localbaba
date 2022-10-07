a664dbd45b30b0fb5b16cafddf6b674c3bfb2ac1
Haseeb Malik <hmalik31@gmail.com>
2022-10-08 03:55:24 +0500

    Added client script

---
+++ dev/install/include/footer.inc.php
@@ -0,0 +1,8 @@
+<div id="footer">
+    <div>
+    &laquo;EasyInstaller &raquo; <?php// echo EI_VERSION;?> &nbsp;<a href="https://scriptorigin.freshdesk.com/">Help</a>
+    <?php if(EI_LICENSE_AGREEMENT_PAGE != ''){ ?>
+        : <a href="<?php echo '../'.EI_LICENSE_AGREEMENT_PAGE;?>" target="_blank"><?php echo lang_key('license'); ?></a>
+    <?php } ?>
+    </div>
+</div>

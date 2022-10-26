0167fcb13f99de7d842ab101996df97cd104ff81
Haseeb Malik <hmalik31@gmail.com>
2022-10-27 00:18:40 +0100

    Added vendor libraries

---
+++ dev/laravel/vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
+use App\Category;
+use App\Setting;
+use App\Page;
+use App\Currency;
+use App\Country;
+use Session;
+
+trait RegistersUsers
+{
+    use RedirectsUsers;
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRegistrationForm()
+    {
+        return view('auth.register')
+		->with('categories',(Category::all()))
+		->with('countries',(Country::all()))
+		->with('pages',Page::all())
+		->with('settings',Setting::first())
+		->with('currencies',Currency::all());
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        $this->guard()->login($user);
+
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        //
+    }
+}
